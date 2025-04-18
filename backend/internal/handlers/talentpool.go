e5c866251340c195beff07146efde6553b707d6b
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2025-04-19 00:36:53 +0700

    stuffs

--- backend/internal/handlers/talentpool.go
+++ backend/internal/handlers/talentpool.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"github.com/iknizzz1807/SkillForge/internal/services"
 )
@@ -14,14 +15,29 @@ func NewTalentPoolHandler(talentPoolService *services.TalentPoolService) *Talent
 	return &TalentPoolHandler{talentPoolService}
 }
 
+// Fix the service of this handler
 func (h *TalentPoolHandler) GetTalentPool(c *gin.Context) {
 	businessID := c.Param("id")
 
 	StudentInfos, err := h.talentPoolService.GetTalentPoolByBusinessID(businessID)
-	
+
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 	c.JSON(http.StatusOK, StudentInfos)
+}
+
+// Dùng để serve POST request cho endpoint /api/talentpool/:id với id là id của student
+// Return StudentInfo với các trường id name và skills là danh sách của các string
+func (h *TalentPoolHandler) AddStudentToTalentPool(c *gin.Context) {
+	studentId := c.Param("id")
+	businessId := c.GetString("userID")
+
+	// Call function from talent pool service to deal with this request
+	StudentInfo, err := h.talentPoolService.AddStudentToTalentPool(studentId, businessId)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot add student to talent pool"})
+	}
+	c.JSON(http.StatusCreated, StudentInfo)
 }
--- backend/internal/services/talentpool_service.go
+++ backend/internal/services/talentpool_service.go
@@ -1,12 +1,6 @@
 package services
 
 import (
-	"errors"
-
-	"context"
-
-	"github.com/iknizzz1807/SkillForge/internal/repositories"
-	"github.com/iknizzz1807/SkillForge/internal/utils"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -20,56 +14,59 @@ func NewTalentPoolService(db *mongo.Database) *TalentPoolService {
 }
 
 type StudentInfo struct {
-	ID        string    `json:"id"`
-	Name      string    `json:"name"`
-	Skills    []string  `json:"skills"`
+	ID     string   `json:"id"`
+	Name   string   `json:"name"`
+	Skills []string `json:"skills"`
 }
 
 func (s *TalentPoolService) GetTalentPoolByBusinessID(businessID string) ([]StudentInfo, error) {
-	if businessID == "" {
-		return nil, errors.New("business id cannot be empty")
-	}
+	// if businessID == "" {
+	// 	return nil, errors.New("business id cannot be empty")
+	// }
 
-	projectRepo := repositories.NewProjectRepository(s.db)
-	projects, err := projectRepo.GetProjectIDsByCreatorID(businessID)
-	if err != nil || projects == nil {
-		return nil, errors.New("projects not found")
-	}
+	// projectRepo := repositories.NewProjectRepository(s.db)
+	// projects, err := projectRepo.GetProjectIDsByCreatorID(businessID)
+	// if err != nil || projects == nil {
+	// 	return nil, errors.New("projects not found")
+	// }
 
-	projectStudentRepo := repositories.NewProjectStudentRepository(s.db)
-	studentIDs := []string{}
-	for _, project := range projects {
-		projectStudentIDs, err := projectStudentRepo.FindStudentsByProjectID(context.Background(), project)
-		if err != nil || projectStudentIDs == nil {
-			return nil, errors.New("student not found")
-		}
-		studentIDs = append(studentIDs, projectStudentIDs...)
-	}
+	// projectStudentRepo := repositories.NewProjectStudentRepository(s.db)
+	// studentIDs := []string{}
+	// for _, project := range projects {
+	// 	projectStudentIDs, err := projectStudentRepo.FindStudentsByProjectID(context.Background(), project)
+	// 	if err != nil || projectStudentIDs == nil {
+	// 		return nil, errors.New("student not found")
+	// 	}
+	// 	studentIDs = append(studentIDs, projectStudentIDs...)
+	// }
 
-	studentIDs = utils.RemoveDuplicates(studentIDs)
+	// studentIDs = utils.RemoveDuplicates(studentIDs)
 
-	StudentInfos := []StudentInfo{}
-	userRepo := repositories.NewUserRepository(s.db)
-	for _, studentID := range studentIDs {
-		userReturn, err := userRepo.FindUserByID(context.Background(), studentID)
-		if err != nil {
-			return nil, err
-		}
+	// StudentInfos := []StudentInfo{}
+	// userRepo := repositories.NewUserRepository(s.db)
+	// for _, studentID := range studentIDs {
+	// 	userReturn, err := userRepo.FindUserByID(context.Background(), studentID)
+	// 	if err != nil {
+	// 		return nil, err
+	// 	}
 
-		id := userReturn.ID
-		name := userReturn.Name
-		skills := userReturn.Skills
+	// 	id := userReturn.ID
+	// 	name := userReturn.Name
+	// 	skills := userReturn.Skills
 
-		StudentInfos = append(StudentInfos, StudentInfo{
-			ID:        id,
-			Name:      name,
-			Skills:    skills,
-		})
-	}
-	
+	// 	StudentInfos = append(StudentInfos, StudentInfo{
+	// 		ID:        id,
+	// 		Name:      name,
+	// 		Skills:    skills,
+	// 	})
+	// }
 
-	if err != nil {
-		return nil, err
-	}
-	return StudentInfos, nil
+	// if err != nil {
+	// 	return nil, err
+	// }
+	// return StudentInfos, nil
+}
+
+func (s *TalentPoolService) AddStudentToTalentPool(studentID string, businessID string) (StudentInfo, error) {
+	// Gọi đến repo để thêm vào database
 }
